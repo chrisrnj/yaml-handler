ac3363b96b0f591defce46971667933aa0be28b0
chrisrnj <christianorangel322@gmail.com>
2025-06-01 12:18:46 -0300

    Add usesConfigurationSectionNodes method, move methods around

--- common/src/main/java/com/epicnicity322/yamlhandler/serializers/CustomSerializer.java
+++ common/src/main/java/com/epicnicity322/yamlhandler/serializers/CustomSerializer.java
@@ -39,12 +39,27 @@ import java.util.Map;
 public interface CustomSerializer<T>
 {
     /**
-     * Whether the nodes can be deserialized with this custom serializer.
+     * Returns the class token representing the type {@code T} this serializer handles.
+     * <p>
+     * This is used internally to associate serializers with specific types and
+     * should accurately reflect the target type.
      *
-     * @param nodes a non-null map of keys and values that might or might not be the object's serialized state
-     * @return if the map can be deserialized as an object of this custom serializer
+     * @return the non-null {@code Class} instance for {@code T}
+     * @since 1.5
      */
-    boolean isDeserializable(@NotNull Map<String, Object> nodes);
+    @NotNull Class<T> type();
+
+    /**
+     * Returns {@code true} if this serializer is compatible with
+     * {@link com.epicnicity322.yamlhandler.ConfigurationSection} values on the serialized {@link Map}.
+     * <p>
+     * This should be {@code false} if the serializer uses plain maps on its deserialization and serialization process.
+     *
+     * @return whether the serializer will use {@link com.epicnicity322.yamlhandler.ConfigurationSection} on the
+     * {@link Map} object of deserialization/serialization
+     * @since 1.5
+     */
+    boolean usesConfigurationSectionNodes();
 
     /**
      * Converts an object of type {@code T} into a map representation that can be stored in YAML.
@@ -55,6 +70,19 @@ public interface CustomSerializer<T>
      * @since 1.5
      */
     @NotNull Map<String, Object> serialize(@NotNull T obj);
+
+    /**
+     * Whether the nodes can be deserialized with this custom serializer.
+     * <p>
+     * The {@code nodes} map will never have inner maps, instead it will have
+     * {@link com.epicnicity322.yamlhandler.ConfigurationSection}, despite the value of
+     * {@link #usesConfigurationSectionNodes()}.
+     *
+     * @param nodes a non-null map of keys and values that might or might not be the object's serialized state
+     * @return if the map can be deserialized as an object of this custom serializer
+     * @since 1.5
+     */
+    boolean isDeserializable(@NotNull Map<String, Object> nodes);
 
     /**
      * Reconstructs an object of type {@code T} from a previously serialized map.
@@ -68,15 +96,4 @@ public interface CustomSerializer<T>
      * @since 1.5
      */
     @NotNull T deserialize(@NotNull Map<String, Object> nodes);
-
-    /**
-     * Returns the class token representing the type {@code T} this serializer handles.
-     * <p>
-     * This is used internally to associate serializers with specific types and
-     * should accurately reflect the target type.
-     *
-     * @return the non-null {@code Class} instance for {@code T}
-     * @since 1.5
-     */
-    @NotNull Class<T> type();
 }
